28e009063aa845632856a23b5227744d20ab1cc3
Shashini Dewmini <dewminihettiarachchi25@gmail.com>
2026-05-31 08:56:36 +0530

    Use remote patterns for Next images

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,18 @@
 import type { NextConfig } from "next";
 
-
 const nextConfig: NextConfig = {
-  /* config options here */
-
- 
   images: {
-    domains: ['images.unsplash.com', 'your-cdn.com'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "your-cdn.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
